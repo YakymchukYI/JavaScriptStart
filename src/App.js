0a5c702d4d19ed30fab5493f8004669ae4620d6a
AndriiHaharin <andriih@aposhealth.com>
2023-02-08 16:10:50 +0200

    any

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("");
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+  const handleForm = (e) => {
+    e.preventDefault();
+    setText(e.target.value)
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{text.toUpperCase()}</p>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control type="text" placeholder="name@example.com" onChange={handleForm}/>
+        </Form.Group>
+        <p>{value}</p>
+        <Button as="a" variant="primary" onClick={handleClick}>
+          ADD
+        </Button>
+        <Button as="a" variant="secondary" onClick={() => setValue(0)}>
+          reset
+        </Button>
       </header>
     </div>
   );
