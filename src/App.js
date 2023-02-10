47eb0a66940a0f0f59fc3a8cb9545a1a02c95353
AndriiHaharin <andriih@aposhealth.com>
2023-02-10 10:47:30 +0200

    create merge conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import './App.css';
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useState } from 'react';
 
 function App() {
-  const [text, setText] = useState("");
   const [value, setValue] = useState(0);
-  const handleClick = () => {
-    setValue(value + 1);
-  };
-  const handleForm = (e) => {
-    e.preventDefault();
-    setText(e.target.value)
-  };
+  const handleClick = () => setValue(value + 1);
+  const handleReset = () => setValue(0);
   return (
     <div className="App">
       <header className="App-header">
-        <p>{text.toUpperCase()}</p>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Control type="text" placeholder="name@example.com" onChange={handleForm}/>
-        </Form.Group>
         <p>{value}</p>
         <Button as="a" variant="primary" onClick={handleClick}>
           ADD
         </Button>
-        <Button as="a" variant="secondary" onClick={() => setValue(0)}>
+        <Button as="a" variant="secondary" onClick={handleReset}>
           reset
         </Button>
       </header>
